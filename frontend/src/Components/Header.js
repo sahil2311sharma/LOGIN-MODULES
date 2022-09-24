0d33e55b8806ce446370a1e91abea34db474eb9a
sahil2311sharma <sahilsharma123027@gmail.com>
2022-09-24 14:23:28 +0530

    update

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -20,7 +20,7 @@ const Header = () => {
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-            <Navbar.Brand>STRIPE</Navbar.Brand>
+            <Navbar.Brand>STRIPE __</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
